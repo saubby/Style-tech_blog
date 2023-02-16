6b2409d766b4264a2dc1ea6d97bc3cc3498e9576
saubby <saubby27@gmail.com>
2023-02-16 15:30:20 -0500

    removed unwanted code

--- server.js
+++ server.js
@@ -1,83 +1,39 @@
-// const path = require('path');
-// const express = require('express');
-// const session = require('express-session');
-// const exphbs = require('express-handlebars');
-// //const routes = require('./controllers');
-// const helpers = require('./utils/helpers');
-// const sequelize = require('./config/connection');
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-// const app = express();
-// const PORT = process.env.PORT || 3001;
-
-// const hbs = exphbs.create({ helpers });
-// const sess = {
-//     secret: 'Super secret secret',
-//     cookie: {
-//         expires: 7200000
-//     },
-//     resave: false,
-//     saveUninitialized: true,
-//     store: new SequelizeStore({
-//         db: sequelize,
-//     }),
-// };
-
-// app.use(session(sess));
-// app.engine('handlebars', hbs.engine);
-// app.set('views', path.join(__dirname, '../views'));
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use(require('./controllers/'));
-
-// sequelize.sync({ force: false }).then(() => {
-//     app.listen(PORT, () => console.log('Now listening on port ' + PORT));
-// });
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
-
 const sess = {
     secret: 'Super secret secret',
     cookie: {
-      maxAge: 300000,
-      httpOnly: true,
-      secure: false,
-      sameSite: 'strict',
+        expires: 7200000
     },
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
-      db: sequelize
-    })
-  };
-  app.use(session(sess));
+        db: sequelize,
+    }),
+};
 
-// Inform Express.js on which template engine to use
+app.use(session(sess));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(routes);
 
+app.use(require('./controllers/'));
+
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+    app.listen(PORT, () => console.log('Now listening on port ' + PORT));
 });
